32d4017546fa792673e2c1e492761bc774e88808
kawsarjama <107822116+kawsarjama@users.noreply.github.com>
2022-11-15 23:05:41 -0800

    upadated

--- main.h
+++ main.h
@@ -2,21 +2,11 @@
 #define MAIN_H
 
 
-#include <stdarg.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <stdbool.h>
 
 
-/*  built infuctions */
-void exit_cmd(char **command, char *line);
 
-/* helper fuctions fuctions */
-void free_buffers(char **buf);
 #endif /* MAIN_H */

511c142f52d89217fe1b5f2f98a254fefb540cdc
kauthar jama <kawsarjaamac37@gmail.com>
2022-11-17 22:41:54 -0800

    commited

--- main.h
+++ main.h
@@ -1,8 +1,37 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+
+
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
+
+
+void exit_cmd(char **command, char *line);
+void free_buffers(char **buf);
+
+
+
+
+
+
+
+
+struct builtin
+{ 
+char *env;  
+char *exit; 
+} builtin;
+
+
 
 #endif /* MAIN_H */
